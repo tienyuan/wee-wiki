1d9a1270e74c6c9e18a193393373a22948cfe09d
Tien Yuan <tien.yuan@gmail.com>
2014-10-28 18:53:23 +0000

    expanded controller wiki and page specs

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,47 +1,95 @@
 require 'rails_helper'
 
-RSpec.describe Wikis::PagesController, :type => :controller do
+describe Wikis::PagesController do
 
   include Devise::TestHelpers
 
   before do
+    @wiki = create(:wiki)
+    @page = create(:page, wiki: @wiki)
     @user = create(:user)
     sign_in @user
   end
 
   describe "#show" do
-    before do
-      @wiki = create(:wiki)
-      @page = create(:page, wiki: @wiki)
-    end
+    render_views
 
-    it "returns http success" do
-      get :show, {wiki_id: @wiki.id, id: @page}
+    it "shows with a valid wiki and page" do
+      get :show, {wiki_id: @wiki.id, id: @page.id}
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include @page.title
+      expect(response.body).to include @page.body
     end
   end
 
   describe "#new" do
-    before do
-      @wiki = create(:wiki)
+    it "shows a new page without saving" do
+      get :new, {wiki_id: @wiki.id}
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)      
+    end
+  end
+
+  describe "#create" do
+    it "creates with valid title and description" do
+      params = {wiki_id: @wiki, page: {title: 'page title', body: 'page body'}}
+      post :create, params
+
+      expect(response).to be_redirect
     end
 
-    it "returns http success" do
-      get :new, {wiki_id: @wiki}
+    it "fails with a blank title" do
+      params = {wiki_id: @wiki, page: {title: '', body: 'page body'}}
+      post :create, params
+
       expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Page failed. Please try again."
+
+    end
+
+    it "fails with a blank description" do
+      params = {wiki_id: @wiki, page: {title: 'page title', body: ''}}
+      post :create, params
+
+      expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Page failed. Please try again."
     end
   end
 
   describe "#edit" do
-    before do
-      @wiki = create(:wiki)
-      @page = create(:page, wiki: @wiki)
-    end
+    it "edits a page" do
+      get :edit, {wiki_id: @wiki.id, id: @page.id}
 
-    it "returns http success" do
-      get :edit, {wiki_id: @wiki.id, id: @page}
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
     end
   end
 
+  describe '#update' do
+    it "updates with valid info" do
+      patch :update, wiki_id: @wiki.id, id: @page.id, page:{title: 'new title'}
+      @page.reload
+
+      expect( response ).to redirect_to wiki_page_path
+    end
+
+    it "fails without a title" do
+      invalid_title = ""
+      patch :update, wiki_id: @wiki.id, id: @page.id, page:{description: invalid_title}
+
+      expect( response ).to redirect_to wiki_page_path
+      expect(flash[:error]).to eq "There was an error updating the page."
+    end
+
+    it "fails without a description" do
+      invalid_body = ""
+      patch :update, wiki_id: @wiki.id, id: @page.id, page:{description: invalid_body}
+
+      expect( response ).to redirect_to wiki_page_path
+      expect(flash[:error]).to eq "There was an error updating the page."
+    end
+  end
 end
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -9,39 +9,99 @@ RSpec.describe WikisController, :type => :controller do
     sign_in @user
   end
 
-  describe "GET index" do
+  describe "#index" do
     it "returns http success" do
       get :index
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:index)
     end
   end
 
-  describe "GET show" do
-    before do
-      @wiki = create(:wiki)
-    end
+  describe "#show" do
+    render_views
 
-    it "returns http success" do
+    it "shows with a valid wiki" do
+      @wiki = create(:wiki)
       get :show, {id: @wiki.id}
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:show)
+      expect(response.body).to include @wiki.title
+      expect(response.body).to include @wiki.description
     end
   end
 
-  describe "GET new" do
-    it "returns http success" do
+  describe "#new" do
+    it "shows a new wiki form" do
       get :new
+
       expect(response).to have_http_status(:success)
+      expect(response).to render_template(:new)      
     end
   end
 
-  describe "GET edit" do
+  describe "#create" do
+    it "creates with valid title and description" do
+      params = {wiki: {title: 'wiki title', description: 'wiki description'}}
+      post :create, params
+
+      expect(response).to be_redirect
+    end
+
+    it "fails with a blank title" do
+      params = {wiki: {title: '', description: 'wiki description'}}
+      post :create, params
+
+      expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Wiki failed. Please try again."
+
+    end
+
+    it "fails with a blank description" do
+      params = {wiki: {title: 'wiki title', description: ''}}
+      post :create, params
+
+     expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Wiki failed. Please try again."
+    end
+  end
+
+  describe "#edit" do
+    it "edits a page" do
+      @wiki = create(:wiki)
+      get :edit, {id: @wiki.id}
+
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe '#update' do
     before do
       @wiki = create(:wiki)
     end
 
-    it "returns http success" do
-      get :edit, {id: @wiki.id}
+    it "updates with valid info" do
+      patch :update, id: @wiki.id, wiki:{title: 'new title'}
+      @wiki.reload
+
+      expect( response ).to be_redirect
+    end
+
+    it "fails without a title" do
+      invalid_title = ""
+      patch :update, id: @wiki.id, wiki:{title: invalid_title}
+
       expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Wiki failed. Please try again."
+    end
+
+    it "fails without a description" do
+      invalid_description = ""
+      patch :update, id: @wiki.id, wiki:{description: invalid_description}
+
+      expect(response).to have_http_status(:success)
+      expect(flash[:error]).to eq "Wiki failed. Please try again."
     end
   end
 
