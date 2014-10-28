1934e0523735bdd87e2a360ca5ee9ba767f1d1f8
Tien Yuan <tien.yuan@gmail.com>
2014-10-28 20:58:13 +0000

    added collab controller spec

---
+++ spec/controllers/collaborations_controller.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+describe Wikis::CollaborationsController do
+
+  include Devise::TestHelpers
+
+  before do
+    @wiki = create(:wiki)
+    @user = create(:user)
+    sign_in @user
+  end
+
+  describe "#create" do
+    it "creates with valid title and description" do
+      params = {wiki_id: @wiki, user: {email: @user.email}}
+      post :create, params
+
+      expect(response).to be_redirect
+      expect(flash[:notice]).to eq "Collaborator added!"
+      expect(User.last.username).to eq(@user.username)
+    end
+
+    it "fails with a blank email" do
+      params = {wiki_id: @wiki, user: {email: ''}}
+      post :create, params
+
+      expect(response).to be_redirect
+      expect(flash[:error]).to eq "Collaborator failed. Please try again."
+      
+    end
+
+    it "fails with a invalid email" do
+      params = {wiki_id: @wiki, user: {email: 'invalid@email.com'}}
+      post :create, params
+
+      expect(flash[:error]).to eq "Collaborator failed. Please try again."
+      
+    end
+  end
+
+  describe '#destroy' do
+    it "deletes with valid info" do
+      @collaboration = create(:collaboration, wiki: @wiki, user: @user)
+      delete :destroy, wiki_id: @wiki.id, id: @user.id
+
+      expect(response).to be_redirect
+      
+      expect(flash[:notice]).to eq "Collaborator removed."
+      expect(Collaboration.count).to eq(0)
+    end
+  end
+end
