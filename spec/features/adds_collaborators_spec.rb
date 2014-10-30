aab622f4b4503cf124118d58523ce18353dc62ea
Tien Yuan <tien.yuan@gmail.com>
2014-10-30 18:07:46 +0000

    fixed wiki index, show and hide collab form from public

--- spec/features/adds_collaborators_spec.rb
+++ spec/features/adds_collaborators_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-feature "User adds collaborators to wiki" do
+feature "User adds collaborators" do
 
   include Warden::Test::Helpers
   Warden.test_mode!
@@ -9,12 +9,13 @@ feature "User adds collaborators to wiki" do
     @user = create(:user)
     @collaborator = create(:user)
     login_as(@user, :scope => :user)
-    @wiki = create(:wiki, title: 'wiki title', description: 'wiki description')
+    @public_wiki = create(:wiki, title: 'public wiki title')
+    @private_wiki = create(:wiki, title: 'private wiki title', description: 'wiki description', private: true, owner: @user)
   end
 
-  scenario "with a valid user email and then remove" do
+  scenario "to private wiki using a valid user email and then removes it" do
     visit wikis_path
-    click_link "wiki title"
+    click_link "private wiki title"
     fill_in 'user-email', with: @collaborator.email
     within 'form' do
       click_button 'Add'
@@ -23,14 +24,14 @@ feature "User adds collaborators to wiki" do
     expect(page).to have_content('Collaborator added!')
     expect(page).to have_content(@collaborator.username)
 
-    click_link "remove"
+    click_link "Remove"
     expect(page).to have_content('Collaborator removed.')
     expect(page).not_to have_content(@collaborator.username)
   end
 
-  scenario "with an invalid user email" do
+  scenario "fails with an invalid user email" do
     visit wikis_path
-    click_link "wiki title"
+    click_link "private wiki title"
     fill_in 'user-email', with: 'invalid@email.com'
     within 'form' do
       click_button 'Add'
@@ -39,15 +40,22 @@ feature "User adds collaborators to wiki" do
     expect(page).to have_content('Collaborator failed.')
   end
 
-  scenario "with a blank email" do
+  scenario "fails with a blank email" do
     visit wikis_path
-    click_link "wiki title"
+    click_link "private wiki title"
     fill_in 'user-email', with: ''
     within 'form' do
       click_button 'Add'
     end
 
     expect(page).to have_content('Collaborator failed.')
+  end
+
+  scenario "fails with a public wiki" do
+    visit wikis_path
+    click_link "public wiki title"
+
+    expect(page).not_to have_content('user-email')
   end
 
   after do
