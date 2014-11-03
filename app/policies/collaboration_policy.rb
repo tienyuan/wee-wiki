eb0a7c2c3405e5702c40c04725a3f1ab92dd77b1
Tien Yuan <tien.yuan@gmail.com>
2014-11-03 04:15:57 +0000

    using pundit helpers

--- app/policies/collaboration_policy.rb
+++ app/policies/collaboration_policy.rb
@@ -2,4 +2,8 @@ class CollaborationPolicy < ApplicationPolicy
   def index
     true
   end
+
+  def create?
+      user.present? && (user == record.wiki.owner || record.wiki.private == false)
+  end
 end
